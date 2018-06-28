c1529659997397cf3d5175f25160a5ae4e7fa7cb
cmcorrales <cmcorrales3@gmail.com>
2018-06-27 21:15:00 -0400

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,14 @@
+require "pry"
 class Hash
-  def keys_of(arguments)
+  def keys_of(*arguments)
     # code goes here
+  animals_array = []
+  self.each do |animals, origin|
+    if arguments.include?(origin)
+      animals_array << animals
+    end
+  end
+  animals_array
   end
 end
+
